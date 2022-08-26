ca7bdf21fcb3d08a2293bb8c820d0fa6a48d41f5
linyiming <2391660155@qq.com>
2022-08-26 11:30:17 +0800

    数组中两元素的最大乘积

--- src/Solution.java
+++ src/Solution.java
@@ -16,45 +16,16 @@ import java.util.List;
 
 
 class Solution {
-    public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        List<Integer> ans = new ArrayList<>();
-        if (x<=arr[0]) {
-            for (int i=0;i<k;i++) {
-                ans.add(arr[i]);
-            }
-            return ans;
-        }
-        int right = findRight(arr,x);
-        int left = right - 1;
-        while (k-->0) {
-            if (right >= arr.length) {
-                left--;
-            } else if (left < 0) {
-                right++;
-            } else if (arr[right]-x >= x-arr[left]) {
-                left--;
-            } else {
-                right++;
+    public int maxProduct(int[] nums) {
+        int a=0,b=0;
+        for (int num:nums) {
+            if (num > a) {
+                b = a;
+                a = num;
+            } else if (num > b){
+                b = num;
             }
         }
-
-
-        for (int i=left+1;i<right;i++) {
-            ans.add(arr[i]);
-        }
-        return ans;
-    }
-
-    public int findRight(int[] example,int x) {
-        int left = 0,right = example.length-1;
-        while (left<right) {
-            int mid = left + (right-left)/2;
-            if (example[mid] >= x) {
-                right = mid;
-            } else {
-                left = mid+1;
-            }
-        }
-        return left;
+        return (a-1)*(b-1);
     }
 }
