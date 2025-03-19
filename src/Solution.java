4e1c05b34c3b901342cdd3e4c1c2f1a22df72c2e
Yiming Lin <2391660155@qq.com>
2025-03-19 18:59:48 +0800

    2610. 转换二维数组

--- src/Solution.java
+++ src/Solution.java
@@ -1,60 +1,20 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-
-//class TreeNode {
-//    int val;
-//    TreeNode left;
-//    TreeNode right;
-//    TreeNode() {}
-//    TreeNode(int val) { this.val = val; }
-//    TreeNode(int val, TreeNode left, TreeNode right) {
-//        this.val = val;
-//        this.left = left;
-//        this.right = right;
-//    }
-// }
-
+import java.util.Map;
 
 class Solution {
-    public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        List<Integer> ans = new ArrayList<>();
-        if (x<=arr[0]) {
-            for (int i=0;i<k;i++) {
-                ans.add(arr[i]);
+    public List<List<Integer>> findMatrix(int[] nums) {
+        int n = nums.length;
+        List<List<Integer>> res = new ArrayList<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], -1) + 1);
+            if (res.size() < map.get(nums[i]) + 1) {
+                res.add(new ArrayList<>());
             }
-            return ans;
+            res.get(map.get(nums[i])).add(nums[i]);
         }
-        int right = findRight(arr,x);
-        int left = right - 1;
-        while (k-->0) {
-            if (right >= arr.length) {
-                left--;
-            } else if (left < 0) {
-                right++;
-            } else if (arr[right]-x >= x-arr[left]) {
-                left--;
-            } else {
-                right++;
-            }
-        }
-
-
-        for (int i=left+1;i<right;i++) {
-            ans.add(arr[i]);
-        }
-        return ans;
-    }
-
-    public int findRight(int[] example,int x) {
-        int left = 0,right = example.length-1;
-        while (left<right) {
-            int mid = left + (right-left)/2;
-            if (example[mid] >= x) {
-                right = mid;
-            } else {
-                left = mid+1;
-            }
-        }
-        return left;
+        return res;
     }
 }
